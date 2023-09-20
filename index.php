428d3aebc56e99dfe23374f08aae4f65a6d602be
masterkudi <cudini.andrea@gmail.com>
2023-09-21 01:08:33 +0200

    impostazione

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . "/classes/petFood.php";
+require_once __DIR__ . "/classes/petKennels.php";
+require_once __DIR__ . "/classes/petToys.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FontAwesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap links -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+    <!-- Axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- Vue.js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- CSS link -->
+    <link rel="stylesheet" href="">
+
+    <!-- Title -->
+
+    <title>PHP-OOP-1</title>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
